0a4688d4ec068925c7c7f0840c41ec4c995daeca
Joshua Hayes-Sheen <grevian+github@gmail.com>
2018-03-13 02:58:31 -0400

    Vendor dependencies with dep

---
+++ vendor/github.com/yuhanfang/riot/constants/lane/lane.go
@@ -0,0 +1,18 @@
+// Package lane defines lane constants.
+package lane
+
+type Lane string
+type Type string
+
+const (
+	Middle Lane = "MIDDLE"
+	Top         = "TOP"
+	Jungle      = "JUNGLE"
+	Bottom      = "BOTTOM"
+)
+
+const (
+	TypeMiddle Type = "MID_LANE"
+	TypeTop         = "TOP_LANE"
+	TypeBottom      = "BOT_LANE"
+)
---
+++ vendor/github.com/yuhanfang/riot/constants/season/season.go
@@ -0,0 +1,17 @@
+// Package season defines season constants.
+package season
+
+type Season int
+
+const (
+	Preseason3 Season = iota
+	Season3
+	Preseason2014
+	Season2014
+	Preseason2015
+	Season2015
+	Preseason2016
+	Season2016
+	Preseason2017
+	Season2017
+)
